f5e7f3b647ef5dee8f421349b3fb89078403f228
Ricardo Fernandes <ricardofernandes@Ricardos-MacBook-Pro.local>
2025-02-01 11:28:50 -0300

    adjust db for windows and changes readme

--- fhir_system/fhir_system/settings.py
+++ fhir_system/fhir_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import platform
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,16 +74,24 @@ WSGI_APPLICATION = 'fhir_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fhir_db',
-        'USER': 'fhir_user',
-        'PASSWORD': 'fhir_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if platform.system() == "Windows":
+        DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'fhir_db',
+            'USER': 'fhir_user',
+            'PASSWORD': 'fhir_password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
