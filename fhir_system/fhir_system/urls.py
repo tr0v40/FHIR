6130dfce071892ed440268666845cac44f64c2b1
Lucas Rocha Moreira <lucas.moreira@math.marketing>
2025-02-11 14:24:22 -0300

    atualização

--- fhir_system/fhir_system/urls.py
+++ fhir_system/fhir_system/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home  # Certifique-se de que a view `home` está correta
+from core.views import home  # Importando a view `home`
 from django.contrib.auth import views as auth_views  # Importa views padrão de autenticação
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),  # Página inicial
     path('core/', include('core.urls')),  # Inclui as rotas do app core
-    path('accounts/', include('django.contrib.auth.urls')),  # Inclui URLs padrão do Django para login/logout
+    path('accounts/', include('django.contrib.auth.urls')),  # URLs padrão do Django para login/logout
 ]
