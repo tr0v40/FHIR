0b13ac7d448d2f044093350d60ac8223a807455d
Lucas Rocha Moreira <lucas.moreira@math.marketing>
2025-02-11 14:24:22 -0300

    atualização

--- fhir_system/core/urls.py
+++ fhir_system/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home  # Importa a view home
+from .views import home, register  # Certifique-se de importar a view register
 
 urlpatterns = [
     path('', home, name='home'),
+    path('register/', register, name='register'),  # Adiciona a rota de cadastro
 ]
--- fhir_system/core/views.py
+++ fhir_system/core/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import Group
+from .forms import UserRegisterForm
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            # Adiciona automaticamente o usuário ao grupo "Usuários Padrão"
+            group, created = Group.objects.get_or_create(name="Usuários Padrão")  # Cria se não existir
+            user.groups.add(group)
+
+            login(request, user)  # Opcional: faz login automático
+            return redirect('home')  # Redireciona para a página inicial
+    else:
+        form = UserRegisterForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
