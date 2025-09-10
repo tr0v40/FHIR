d0ed7cacfff4c0fb65ddce15a32ab3b0668f3e50
Lucas Rocha Moreira <lucas.moreira@math.marketing>
2025-09-10 19:01:21 -0300

    ajustes

--- fhir_system/fhir_system/settings.py
+++ fhir_system/fhir_system/settings.py
@@ -39,7 +39,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Instalar apps
 INSTALLED_APPS = [
     "jazzmin",
-    'django_extensions'
+    'django_extensions',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
