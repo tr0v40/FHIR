24e2306b06e190616c80506c00b2cbee4f3e7813
Lucas Rocha Moreira <lucas.moreira@math.group>
2026-05-20 18:14:59 -0300

    Adicao das paginas de erros em ingles v2

--- fhir_system/core/error_views.py
+++ fhir_system/core/error_views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
 
 
-
-def custom_404(request, exception):
-
-    path = request.path.lower()
-
-    english_routes = [
-        "/treatments/",
-        "/trestments/",
-        "/treatment/",
-        "/treat/"
-    ]
-
-    if any(route in path for route in english_routes):
-        return render(request, "404_en.html", status=404)
-
-    return render(request, "404.html", status=404)
+def error_404_en(request, exception):
+    return render(
+        request,
+        "404_en.html",
+        status=404
+    )
 
 
 def error_500_en(request):
